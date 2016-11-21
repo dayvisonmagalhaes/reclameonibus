aba3e1b9640cf6a55fc9e9104d851ab6ab40074c
matheusnavega <m.navega6@gmail.com>
2016-11-21 18:33:24 -0200

    Delete connection.php

--- API/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$conn = mysql_connect("matheusnavega.com.br", "matheusn_admin", "reclameonibus06");
-	mysql_select_db("matheusn_reclameonibus",$conn);
-	mysql_set_charset("UTF8");
-
-	header ('Content-type: application/json; charset=utf-8');
-
-?>
